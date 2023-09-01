3f340223d54245702d7e20d1949f95da3ba6fb15
Jasyd Caballero <jasydavid45@gmail.com>
2023-08-31 23:29:27 -0500

    add functions for printing strings, characters and number in some bases

---
+++ src/kernel/stdio.c
@@ -0,0 +1,123 @@
+#include <stdint.h>
+#include "math.h"
+
+#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 25
+#define VGA_BUFFER_ADDRESS 0xB8000
+
+// Possible background and foreground colors
+enum colors {
+  BLACK,
+  BLUE,
+  GREEN,
+  CYAN,
+  RED,	
+  MAGENTA,
+  BROWN,
+  LIGHT_GREY,
+  DARK_GREY,
+  LIGHT_BLUE,
+  LIGHT_GREEN,
+  LIGHT_CYAN,
+  LIGHT_RED,
+  LIGHT_MAGENTA,
+  LIGHT_BROWN,
+  WHITE,
+};
+
+// Current location in the screen
+int currX = 0, currY = 0;
+uint8_t *textModeVGAPtr;
+
+void putc (unsigned char c) {
+  int offset = ((SCREEN_WIDTH * currY) + currX);
+  uint16_t *location = (uint16_t *)textModeVGAPtr + offset;
+
+  uint8_t backcolor = BLUE;
+  uint8_t forecolor = WHITE;
+
+  if (c == '\n') {
+    currX = 0;
+    currY++;
+  }
+  else {
+    // 15            12 11            8 7              0
+    // -------------------------------------------------
+    // |   Backcolor   |   Forecolor   |   Character   |
+    // -------------------------------------------------
+    *location = backcolor << 12 | forecolor << 8 | c;
+    currX++;
+  }
+
+  /* If the cursor has reached the edge of the screen's width, 
+   * we insert a new line in there */
+  if (currX > SCREEN_WIDTH) {
+    currX = 0;
+    currY++;
+  }
+}
+
+
+void hScroll () {
+  // TODO: memset
+}
+
+void cls (void) {
+  uint8_t *vga = textModeVGAPtr;
+  // TODO: change to memset
+  for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT * 2; i++) 
+    *++vga = 0x0;
+
+  // Update virtual cursor
+  currX = 0;
+  currY = 0;
+
+  // Move hardware cursor
+  // TODO: moveCsr();
+}
+
+void puts (char *string) {
+  while (*string) putc(*string++);
+}
+
+void putNumber (int number, int base) {
+  char *numbers = "0123456789ABCDEF";
+  
+  char *prefix;
+  switch (base) {
+    case 2: {
+      prefix = "0b";
+      break;
+    }
+    case 16: {
+      prefix = "0x";
+      break;
+    }
+    case 10:
+    default: {
+      prefix = "";
+    }
+  }
+
+  if (number < 0) putc('-');
+  puts(prefix);
+  number = abs(number);
+
+  char *digits = "";
+
+  int i = 0;
+  while (number > 0) {
+    uint8_t digit = number % base;
+    digits[i++] = numbers[digit];
+    number /= base;
+  }
+
+  while (i--) {
+    putc(digits[i]);
+  }
+}
+
+void initVideo (void) {
+  textModeVGAPtr = (uint8_t *)VGA_BUFFER_ADDRESS;
+  cls();
+}
---
+++ src/kernel/stdio.h
@@ -0,0 +1,19 @@
+/*
+ * Print the given string to the screen
+ */
+void puts (char *string);
+
+/*
+ * Print the given number in the specified base to the screen
+ */
+void putNumber (int number, int base);
+
+/*
+ * Clear the whole screen
+ */
+void cls (void);
+
+/*
+ * Set the text-mode VGA pointer, then clears the screen
+ */
+void initVideo (void);
