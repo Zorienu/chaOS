b6337bff724994330f614760c941378009430852
Jasyd Caballero <jasydavid45@gmail.com>
2023-09-01 23:50:18 -0500

    testing memcpy, memset and strlen

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,15 +1,36 @@
 #include <stdint.h>
 #include "stdio.h"
 #include "string.h"
+#include "mem.h"
 
 
 void OSStart() {
   char string[] = "Lore ipsum";
+  char* string2 = "Ipsum lore"; 
 
   initVideo();
+  putNumber(255, 10);
+  puts("\n");
   putNumber(511, 16);
   puts("\n");
   putNumber(255, 2);
   puts("\n");
   putNumber(255, 16);
+
+  puts("\n");
+  puts(string);
+  puts("\n");
+  puts(string2);
+  puts("\n");
+  memset(&string, 'H', 4);
+  puts("\n");
+  puts(string);
+  puts("\n");
+  puts(string2);
+  puts("\n");
+  memcpy(string2, string, strlen(string));
+  puts("\n");
+  puts(string);
+  puts("\n");
+  puts(string2);
 }
