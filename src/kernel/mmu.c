5b5451b0ab05b0cf5fbb295edf9c192a85556b17
Jasyd Caballero <jasydavid45@gmail.com>
2023-09-13 23:49:28 -0500

    add minimum page directory for enabling paging

---
+++ src/kernel/mmu.c
@@ -0,0 +1,12 @@
+#include "mmu.h"
+
+typedef uint32_t PageDirectoryEntry;
+
+/*
+ * Map the first and last 4MiB of virtual addresses to first 4MiB of physical adresses
+ * Here we use 4MiB pages for simplicity
+ */
+PageDirectoryEntry minimalPageDirectory[PAGE_DIRECTORY_ENTRIES]  __attribute__((aligned(PAGE_SIZE))) = {
+  [0] = (0) | PAGE_SIZE_4_MB | READ_AND_WRITE_PAGE | PRESENT_PAGE,
+  [PAGE_DIRECTORY_ENTRIES - 1] = (0) | PAGE_SIZE_4_MB | READ_AND_WRITE_PAGE | PRESENT_PAGE,
+};
