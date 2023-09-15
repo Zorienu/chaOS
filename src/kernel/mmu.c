bf782bf00c36991e841c98815fd878d68b9ea0b7
Jasyd Caballero <jasydavid45@gmail.com>
2023-09-14 23:49:57 -0500

    adjust entryPageDirectory, set the 2 GiB VA to point to 0 PA

--- src/kernel/mmu.c
+++ src/kernel/mmu.c
@@ -1,4 +1,5 @@
 #include "mmu.h"
+#include "memLayout.h"
 
 typedef uint32_t PageDirectoryEntry;
 
@@ -6,9 +7,9 @@ typedef uint32_t PageDirectoryEntry;
  * Map the first and last 4MiB of virtual addresses to first 4MiB of physical adresses
  * Here we use 4MiB pages for simplicity
  */
-PageDirectoryEntry minimalPageDirectory[PAGE_DIRECTORY_ENTRIES]  __attribute__((aligned(PAGE_SIZE))) = {
+PageDirectoryEntry entryPageDirectory[PAGE_DIRECTORY_ENTRIES]  __attribute__((aligned(PAGE_SIZE))) = {
   [0] = (0) | PAGE_SIZE_4_MB | READ_AND_WRITE_PAGE | PRESENT_PAGE,
-  [PAGE_DIRECTORY_ENTRIES - 1] = (0) | PAGE_SIZE_4_MB | READ_AND_WRITE_PAGE | PRESENT_PAGE,
+  [KERNEL_BASE >> PAGE_DIRECTORY_SHIFT] = (0) | PAGE_SIZE_4_MB | READ_AND_WRITE_PAGE | PRESENT_PAGE,
 };
 
 void loadPageDirectory(PageDirectoryEntry *pageDirectory) {
