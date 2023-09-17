1df323bcf2e2f928470ff6ad700e9a6a254e6d7c
Jasyd Caballero <jasydavid45@gmail.com>
2023-09-16 23:33:33 -0500

    add functions for align up and down a given address

--- src/kernel/memLayout.c
+++ src/kernel/memLayout.c
@@ -1,4 +1,6 @@
 #include "memLayout.h"
+#include "mmu.h"
+#include <stdint.h>
 
 void *V2P(void *va) {
   return va - KERNEL_BASE;
@@ -6,4 +8,12 @@ void *V2P(void *va) {
 
 void *P2V(void *pa) {
   return pa + KERNEL_BASE;
+}
+
+uint32_t ALIGN_ADDRESS_UP(uint32_t address) {
+  return (address + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1);
+}
+
+uint32_t ALIGN_ADDRESS_DOWN(uint32_t address) {
+  return address & ~(PAGE_SIZE - 1);
 }
--- src/kernel/memLayout.h
+++ src/kernel/memLayout.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 #define KERNEL_BASE 0x80000000 // 2 GiB
 #define IO_SPACE    0x100000 
 #define PHYSICAL_STOP 0xE000000 // Total physical memory - 224 MiB
@@ -16,3 +18,24 @@ void *V2P(void *va);
  * Convert the given physical address to virtual address
  */
 void *P2V(void *pa);
+
+/* 
+ * Align up the given address
+ * @param {uint32_t} address - The address we want to align up
+ * @example 0x1000 -> 0x1000
+ * @example 0x1001 -> 0x2000
+ * @example 0x1FFF -> 0x2000
+ * @example 0x2000 -> 0x2000
+ */
+uint32_t ALIGN_ADDRESS_UP(uint32_t address);
+
+
+/* 
+ * Align down the given address
+ * @param {uint32_t} address - The address we want to align down
+ * @example 0x1000 -> 0x1000
+ * @example 0x1001 -> 0x1000
+ * @example 0x1FFF -> 0x1000
+ * @example 0x2000 -> 0x2000
+ */
+uint32_t ALIGN_ADDRESS_DOWN(uint32_t address);
