9878d0282e6a931431ecd870f87462a0b7b5c59d
Jasyd Caballero <jasydavid45@gmail.com>
2023-09-23 23:50:21 -0500

    add page 1 to entry page directory

--- src/kernel/mmu.c
+++ src/kernel/mmu.c
@@ -8,8 +8,14 @@ typedef uint32_t PageDirectoryEntry;
  * Here we use 4MiB pages for simplicity
  */
 PageDirectoryEntry entryPageDirectory[PAGE_DIRECTORY_ENTRIES]  __attribute__((aligned(PAGE_SIZE))) = {
+  // 0x0 to 0x400000 - 1
   [0] = (0) | PAGE_SIZE_4_MB | READ_AND_WRITE_PAGE | PRESENT_PAGE,
+  // 0x400000 to 0x800000 - 1
+  [1] = (0x400000) | PAGE_SIZE_4_MB | READ_AND_WRITE_PAGE | PRESENT_PAGE,
+  // 0x0 to 0x400000 - 1
   [KERNEL_BASE >> PAGE_DIRECTORY_SHIFT] = (0) | PAGE_SIZE_4_MB | READ_AND_WRITE_PAGE | PRESENT_PAGE,
+  // 0x400000 to 0x800000 - 1
+  [(KERNEL_BASE >> PAGE_DIRECTORY_SHIFT) + 1] = (0x400000) | PAGE_SIZE_4_MB | READ_AND_WRITE_PAGE | PRESENT_PAGE,
 };
 
 void loadPageDirectory(PageDirectoryEntry *pageDirectory) {
