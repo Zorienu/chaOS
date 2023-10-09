ce50c8f86a3fed4a49d3344e48a42114eb19e2c6
Jasyd Caballero <jasydavid45@gmail.com>
2023-10-08 22:28:46 -0500

    add type definitions and enums for paging

--- src/include/mem/virtualMem.h
+++ src/include/mem/virtualMem.h
@@ -1,3 +1,58 @@
+#include <stdint.h>
+
+#define MB 0x100000
+#define PAGE_SIZE 4096
+#define PAGES_PER_TABLE 1024
+#define TABLES_PER_DIRECTORY 1024
+
+#define PD_INDEX(address) ((address) >> 22)
+#define PT_INDEX(address) ((address) >> 12) & 0x3FF // 0x3FF -> 1023
+#define PAGE_PHYSICAL_ADDRESS (directoryEntry) ((*directoryEntry) & ~0xFFF)
+#define REMOVE_ATTRIBUTE (entry, attribute) (*entry &= ~attribute)
+#define TEST_ATTRIBUTE (entry, attribute) (*entry & attribute)
+
+typedef uint32_t PageDirectoryEntry;
+typedef uint32_t PageTableEntry;
+typedef uint32_t VirtualAddress;
+typedef uint32_t PhysicalAddress;
+
+typedef enum {
+    PTE_PRESENT       = 0x01,
+    PTE_READ_WRITE    = 0x02,
+    PTE_USER          = 0x04,
+    PTE_WRITE_THROUGH = 0x08,
+    PTE_CACHE_DISABLE = 0x10,
+    PTE_ACCESSED      = 0x20,
+    PTE_DIRTY         = 0x40,
+    PTE_PAT           = 0x80,
+    PTE_GLOBAL        = 0x100,
+    PTE_FRAME         = 0x7FFFF000,   // bits 12+
+} PAGE_TABLE_FLAGS;
+
+typedef enum {
+    PDE_PRESENT       = 0x01,
+    PDE_READ_WRITE    = 0x02,
+    PDE_USER          = 0x04,
+    PDE_WRITE_THROUGH = 0x08,
+    PDE_CACHE_DISABLE = 0x10,
+    PDE_ACCESSED      = 0x20,
+    PDE_DIRTY         = 0x40,          // 4MB entry only
+    PDE_PAGE_SIZE     = 0x80,          // 0 = 4KB page, 1 = 4MB page
+    PDE_GLOBAL        = 0x100,         // 4MB entry only
+    PDE_PAT           = 0x2000,        // 4MB entry only
+    PDE_FRAME         = 0x7FFFF000,    // bits 12+
+} PAGE_DIRECTORY_FLAGS;
+
+
+// Page table: handle 4MB each, 1024 entries * 4096
+typedef struct {
+    PageTableEntry entries[PAGES_PER_TABLE];
+} PageTable;
+
+// Page directory: handle 4GB each, 1024 page tables * 4MB
+typedef struct {
+    PageDirectoryEntry entries[TABLES_PER_DIRECTORY];
+} PageDirectory;
 /*
  * Set the given paging attribute to the given page table entry
  */
