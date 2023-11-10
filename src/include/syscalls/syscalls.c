53bf64e0a58d2a229add3c21ab7f59478fba7a67
Jasyd Caballero <jasydavid45@gmail.com>
2023-11-09 22:20:32 -0500

    add syscall dispatcher, called when the 0x80 interrupt occurs

---
+++ src/include/syscalls/syscalls.c
@@ -0,0 +1,57 @@
+#include "syscalls.h"
+
+/*
+ * Syscall table
+ */
+int32_t (*syscalls[10])(SyscallRegisters) = {
+  [0] = syscallTest,
+};
+
+int32_t syscallTest(SyscallRegisters regs) {
+  return 10 + 20;
+}
+
+__attribute__ ((naked)) void syscallDispatcher(void) {
+  asm volatile (".intel_syntax noprefix\n"
+
+                ".equ MAX_SYSCALLS, 10\n"     // Have to define again, inline asm does not see the #define
+
+                "cmp eax, MAX_SYSCALLS-1\n"   // syscalls table is 0-based
+                "ja invalid_syscall\n"        // invalid syscall number, skip and return
+
+                "push eax\n"
+                "push gs\n"
+                "push fs\n"
+                "push es\n"
+                "push ds\n"
+                "push ebp\n"
+                "push edi\n"
+                "push esi\n"
+                "push edx\n"
+                "push ecx\n"
+                "push ebx\n"
+                "push esp\n"
+                "call [syscalls + eax * 4]\n"
+                "add esp, 4\n"    // Do not overwrite esp
+                "pop ebx\n"
+                "pop ecx\n"
+
+                // Do not overwrite EDX, as some syscalls e.g. malloc() will need it
+                "add esp, 4\n"    
+
+                "pop esi\n"
+                "pop edi\n"
+                "pop ebp\n"
+                "pop ds\n"
+                "pop es\n"
+                "pop fs\n"
+                "pop gs\n"
+                "add esp, 4\n"    // Save eax value in case; don't overwrite it
+                "iretd\n"         // Need interrupt return here! iret, NOT ret
+
+                "invalid_syscall:\n"
+                "mov eax, -1\n"   // Error will be -1
+                "iretd\n"
+
+                ".att_syntax");
+} 
---
+++ src/include/syscalls/syscalls.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <stdint.h>
+
+/*
+ * Registers pushed onto stack when a syscall function is called
+ * from the syscall dispatcher
+ */
+typedef struct {
+    uint32_t esp;
+    uint32_t ebx;
+    uint32_t ecx;
+    uint32_t edx;
+    uint32_t esi;
+    uint32_t edi;
+    uint32_t ebp;
+    uint32_t ds;
+    uint32_t es;
+    uint32_t fs;
+    uint32_t gs;
+    uint32_t eax;
+} __attribute__ ((packed)) SyscallRegisters;
+
+/*
+ * Test syscall implementation
+ */
+int32_t syscallTest(SyscallRegisters regs);
+
+
+/*
+ * Syscall dispatcher, called when a syscall is invoked
+ * "naked" means: do not add function epilogue/prologue, and only allow inline asm
+ *
+ * Push everything we want to save
+ * Call the syscall by offseting into the syscall table by using the value in eax
+ * Pop everything back
+ * Return using "iret" since it's an interrupt (software interrupt)
+ *
+ * Already on stack: SS, SP, FLAGS, CS, IP
+ * Need to push: AX, GS, FS, ES, DS, BP, DI, SI, DX, CX, BX
+ */
+__attribute__ ((naked)) void syscallDispatcher(void); 
