e0ba03a9152246c3575f9213400840801c399d81
Jasyd Caballero <jasydavid45@gmail.com>
2023-11-11 22:27:01 -0500

    use syscall number in syscall test

--- src/include/sys/syscallWrappers.c
+++ src/include/sys/syscallWrappers.c
@@ -1,10 +1,13 @@
-#include "./syscallWrappers.h"
 #include <stdint.h>
+#include "./syscallWrappers.h"
+#include "../syscalls/syscallNumbers.h"
 
 int32_t syscallTestWrapper() {
   int32_t result = -1;
 
-  asm volatile("int $0x80" : "=a"(result) : "a"(0));
+  asm volatile("int $0x80" : "=a"(result) : "a"(SYSCALL_TEST));
 
   return result;
 }
+
+
