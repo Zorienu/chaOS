990e8b71f0d6da22880d5b0e3f0cac0ff343bc46
Jasyd Caballero <jasydavid45@gmail.com>
2023-11-11 22:28:13 -0500

    add basic malloc (TODO: allocate more pages when available bytes are not enought for further allocations)

---
+++ src/include/mem/malloc.c
@@ -0,0 +1,71 @@
+#include <stdint.h>
+#include "malloc.h"
+#include "mem.h"
+#include "virtualMem.h"
+#include "../c/stdio.h"
+
+MallocBlockType *mallocListHead = 0;
+VirtualAddress mallocVirtualAddress = 0;
+PhysicalAddress mallocPhysicalAddress = 0;
+
+void mallocInit(uint32_t size) {
+  mallocPhysicalAddress = (PhysicalAddress)allocateBlock();
+  mallocVirtualAddress = 0x300000;
+
+  mapPage(mallocVirtualAddress, mallocPhysicalAddress);
+
+  mallocListHead = (MallocBlockType *)mallocVirtualAddress;
+
+  mallocListHead->size = PAGE_SIZE - sizeof(MallocBlockType);
+  mallocListHead->free = true;
+  mallocListHead->next = 0;
+}
+
+void mallocSplit(MallocBlockType *temp, uint32_t size) {
+  MallocBlockType *newBlock = (void *)temp + sizeof(MallocBlockType) + size;
+  printf("\n-->Temp address: %lx", temp);
+  printf("\n-->New block: %lx", newBlock);
+
+  newBlock->free = true;
+  newBlock->size = temp->size - size - sizeof(MallocBlockType);
+  newBlock->next = 0;
+
+  temp->next = newBlock;
+  temp->free = false;
+  temp->size = size;
+
+  printf("\n-->Old block size: %d", temp->size);
+  printf("\n-->New block size: %d", newBlock->size);
+}
+
+void *mallocNextBlock(uint32_t size) {
+  // TODO: review if this is ok
+  if (!mallocListHead) mallocInit(size);
+
+  // Nothing to malloc
+  if (!size) return 0;
+
+  MallocBlockType *temp = mallocListHead;
+
+  printf("\n-->Next block: %lx", temp->next);
+
+  // Find next big enough free block
+  while (((temp->size < size) || !temp->free) && temp->next) {
+    printf("\nInside while: %lx", temp);
+    temp = temp->next;
+  }
+
+  printf("\nFree block: %lx", temp);
+
+  if (size == temp->size && temp->free) {
+    temp->free = false;
+  }
+  else if (temp->size > size + sizeof(MallocBlockType)) {
+    mallocSplit(temp, size);
+    printf("\nAfter malloc split: temp.next: %lx", temp->next);
+  } else {
+    return 0;
+  }
+
+  return (void *)temp + sizeof(MallocBlockType);
+}
---
+++ src/include/mem/malloc.h
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct MallocBlock { 
+  uint32_t size;                // Size in bytes of the block
+  bool free;                    // Whether the block is free or not
+  struct MallocBlock *next;     // Next block
+} MallocBlockType;
+
+/*
+ * Allocate the next free block of memory
+ */
+void *mallocNextBlock(uint32_t size);
