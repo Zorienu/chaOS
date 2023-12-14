7f00d6736c571a9010b711c1202c4f80564429b6
Jasyd Caballero <jasydavid45@gmail.com>
2023-12-13 22:06:33 -0500

    rename and comment new and delete operator using malloc, they will be used for user-space in the future

--- src/include/mem/kmalloc_old.cpp
+++ src/include/mem/kmalloc_old.cpp
@@ -1,6 +1,11 @@
 #include <stddef.h>
 #include "../c/stdlib.h"
 
+/*
+ * TODO: compile and link only for user-space, 
+ * for kernel there is the kmalloc and kfree implementation in kernel/heap/kmalloc.cpp
+ */
+/*
 void *operator new(size_t size)
 {
     return malloc(size);
@@ -20,3 +25,4 @@ void operator delete[](void *p)
 {
     free(p);
 }
+*/
