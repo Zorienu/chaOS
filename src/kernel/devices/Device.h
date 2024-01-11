59cd731209977891c7da50dcca6331ec3e04b856
Jasyd Caballero <jasydavid45@gmail.com>
2024-01-11 12:08:32 -0500

    mark isDevice method as final in Device class

--- src/kernel/devices/Device.h
+++ src/kernel/devices/Device.h
@@ -3,7 +3,7 @@
 
 class Device : public File {
   public:
-    virtual bool isDevice() const override { return true; }
+    virtual bool isDevice() const override final { return true; }
 
   protected:
     Device() {};
