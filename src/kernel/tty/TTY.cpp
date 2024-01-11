5366ea7f4464726b4bbf2d22d26884b4349f4221
Jasyd Caballero <jasydavid45@gmail.com>
2024-01-11 12:06:31 -0500

    add definitions for TTY methods

--- src/kernel/tty/TTY.cpp
+++ src/kernel/tty/TTY.cpp
@@ -4,3 +4,19 @@ void TTY::setSize(uint8_t rows, uint8_t columns) {
   _rows = rows;
   _columns = columns;
 }
+
+bool TTY::canRead(FileDescription&) const {
+  return true;
+}
+
+size_t TTY::read(FileDescription&, uint8_t *buffer, size_t size) {
+  return size;
+}
+
+bool TTY::canWrite(FileDescription&) const {
+  return false;
+}
+
+size_t TTY::write(FileDescription&, const uint8_t *buffer, size_t size) {
+  return size;
+}
--- src/kernel/tty/TTY.h
+++ src/kernel/tty/TTY.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "stdint.h"
+#include <stddef.h>
 #include "../devices/CharacterDevice.h"
-#include <cstddef>
 
 class TTY : public CharacterDevice {
   public:
