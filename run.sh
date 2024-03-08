402b1cdc35578301b667e9151094f3e1dc80c716
Jasyd Caballero <jasydavid45@gmail.com>
2024-03-08 00:04:50 -0500

    add monitor in qemu

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 # qemu-system-i386 -fda build/bin/boot.img
-qemu-system-i386 -hda build/bin/boot.img
+qemu-system-i386 -monitor stdio -drive format=raw,file=build/bin/boot.img,if=ide,index=0,media=disk # -d int -no-reboot
