1985a581f0f659f01a5428af431f6f8f1a03972d
Jasyd Caballero <jasydavid45@gmail.com>
2024-03-19 21:49:48 -0500

    organize superBlock struct

--- src/kernel/fileSystem/fs.h
+++ src/kernel/fileSystem/fs.h
@@ -3,20 +3,34 @@
 
 #define BLOCK_SIZE 4096
 
+#define ROOT_DIRECTORY_INODE 1
+
 enum FileType : uint8_t {
   FILETYPE_FILE = 0,
   FILETYPE_DIRECTORY = 1,
 };
 
 struct superBlock {
+  // Inode bitmap
   uint32_t totalNumberOfInodes;
-  uint32_t totalNumberOfDataBlocks; 
   uint32_t firstInodesBitmapBlock;  // What is the first block containing the bitmap for INodes?
+  uint32_t inodesBitmapBlocks; // How many blocks the inode bitmap take?
+
+  // Data blocks bitmap
+  uint32_t totalNumberOfDataBlocks; // How many disk blocks are in the image? (including bootsector, superblock and bitmaps)
   uint32_t firstDataBlocksBitmapBlock; // What is the first block containing the bitmap for data blocks?
+  uint32_t dataBlocksBitmapBlocks; // Hoy many blocks the data bitmap take?
+
+  // Inode table
   uint32_t firstInodeBlock; // What is the first block containing the Inodes?
+  uint32_t inodesBlocks; // How many blocks the inode table take?
+
+  // Data blocks (where the actual data is)
   uint32_t firstDataBlock; // What is the first block containing the data blocks?
+
   uint16_t blockSizeInBytes; // What is the size of each block? Should be BLOCK_SIZE
   uint16_t inodeSizeInBytes; // What is the size of each INode? Should be sizeof(struct inode)
+  uint32_t maxFileSize;
 };
 
 struct inode {
