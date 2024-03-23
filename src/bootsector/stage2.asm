2bdea194c300259930e5b258a5828838aa7837b7
Jasyd Caballero <jasydavid45@gmail.com>
2024-03-22 23:56:07 -0500

    move stage2 again to bootsector binary

--- src/bootsector/stage2.asm
+++ src/bootsector/stage2.asm
@@ -15,13 +15,7 @@ bits 16
 section .stage2
 jmp stage2
 
-; 
-; Text section will be put in 0xB000 to avoid memory map addresses
-;
-section .text
-halt:
-  cli
-  hlt
+extern halt
 
 ; -----------------------------------------------------------------------------------------------
 ; 
@@ -130,9 +124,153 @@ enterProtectedMode:
   call halt
 
 
+%define DISK_PORT_BASE 0x1F0
+%define SECTOR_SIZE 512
+%define DISK_READ_CMD 0x20
+
+; 
+; Wait for the hard disk to be ready
+;
+bits 32
+wait_disk:
+  push dx
+  push ax
+
+.loop:
+  mov dx, DISK_PORT_BASE + 7 
+  in al, dx
+  and al, 0xC0
+  cmp al, 0x40
+  jne .loop
+
+.done: 
+  pop ax
+  pop dx
+
+  ret
+
+; 
+; Read sectors from disk using IO
+; Parameters:
+;   - bx: Sector number to read
+;   - di: Memory address where to store the read data
+;   - cx: Amount of sectors to read
+;
+bits 32
+disk_read:
+  ; Save register we will modify
+  push ax 
+  push bx
+  push cx
+  push dx
+  push di
+
+  ; Wait for the disk
+  call wait_disk
+
+  ; Save sector number to read
+  ; push bx
+
+  ; Convert number of bytes to number of sectors to read
+  ; Number of sectors to read will be in ax and remainder in dx
+  ; mov ax, cx
+  ; mov bx, SECTOR_SIZE
+  ; cwd ; prepare the DX:AX register pair for division -> dx will be set to 0
+      ;  setting dx to 0 manually will work too
+  ; div bx
+
+  ; Restore sector number to read
+  ; pop bx
+
+  ; Save amount of sectors to read... for later
+  push cx
+
+  ; Define number of sectors to read
+  mov dx, DISK_PORT_BASE + 2
+  mov ax, cx
+  out dx, al
+
+  ; Define sector to read
+  mov dx, DISK_PORT_BASE + 3
+  mov al, bl ; LSB of bx
+  out dx, al
+
+  mov dx, DISK_PORT_BASE + 4
+  mov al, bh ; MSB of bx
+  out dx, al
+
+  mov dx, DISK_PORT_BASE + 5
+  mov al, 0 ; sector 0
+  out dx, al
+
+  mov dx, DISK_PORT_BASE + 6
+  mov al, 0xE0 ; 0xE0 -> 0b1110_0000
+               ; bit 4: drive number (0 in our case)
+               ; bit 5: always set
+               ; bit 6: set for LBA
+               ; bit 7: always set
+  or al, 0 ; sector 0
+  out dx, al
+
+  ; Send read command
+  mov dx, DISK_PORT_BASE + 7
+  mov al, DISK_READ_CMD
+  out dx, al
+
+  ; Wait for the disk
+  call wait_disk
+
+  ; The division puts the remainder in the edx register, so
+  ; we have to tell the disk port after the division
+  mov dx, DISK_PORT_BASE ; Port to read
+
+  ; cld
+  ; ; rep insd ; Reading DWORDS (32 bits) repeatedly until cx (number of times we have to read) reaches 0
+  ; rep insw ; Reading WORDS (16 bits) repeatedly until cx (number of times we have to read) reaches 0
+
+  ; Restore previously saved "amount of sectors to read" into bx
+  pop bx
+
+; Loop for every sector we're gonna read
+; NOTE: this worked with cld; rep insw and cx holding the total amount of bytes to read / 2 (4 in case of insd)
+; in bochs but not in qemu. So, we had to read sectors one by one adding a wait for disk
+.loop:
+  ; How many times are we gonna read from the port?
+  ; cx register will contain the counter...
+  ; insd reads 32 bits at a time (4 bytes)
+  ; insw reads 16 bits at a time (2 bytes)
+  ; so, we have to divide the amount of bytes to read by 4 or 2 depending of the used command
+  mov cx, SECTOR_SIZE / 2
+
+  cld
+  ; rep insd ; Reading DWORDS (32 bits) repeatedly until cx (number of times we have to read) reaches 0
+  rep insw ; Reading WORDS (16 bits) repeatedly until cx (number of times we have to read) reaches 0
+
+  call wait_disk
+
+  ; Have we read all the sectors?
+  cmp bl, 0
+  je .done
+
+  ; We read 1 sector, then, decrement the counter
+  dec bx
+  jmp .loop
+
+.done:
+  ; Restore registers we modified
+  pop di 
+  pop dx
+  pop cx
+  pop bx
+  pop ax
+
+  ret
+
+
 ;
 ; Already in protected mode
 ;
+%define PREKERNEL_MEMORY_ADDRESS 0x50000
 bits 32
 PModeMain:
   ; Setup segments register to 16 (0x10) which is the offset within the GDT to use the kernel mode data segment
@@ -150,7 +288,12 @@ PModeMain:
   ;
   mov esp, 0x7C00
 
-  call OSEntry
+  mov edi, PREKERNEL_MEMORY_ADDRESS ; di will contain the destination address of the read sectors
+  mov cx, 50 ;* SECTOR_SIZE ; cx will contain the number of bytes to read (60 sectors)
+  mov bx, 48 ; from which sector we want to read?
+  call disk_read
+
+  call PREKERNEL_MEMORY_ADDRESS
 ;
 ; End of protected mode configuration
 ;
@@ -373,17 +516,5 @@ GDTEnd:
 GDTDescriptor:
 .GDTSize:    dw GDTEnd - GDTStart
 .GDTAddress: dd GDTStart
-
-; OS bootloader code (bootmain.c)
-bits 32
-
-extern loadOS
-
-section .text
-
-OSEntry:
-  call loadOS
-  cli 
-  hlt
 
 times 512 - ($ - $$) db 0
